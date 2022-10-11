eb8f57661fcbf7f339797eb2010aaa7f21bd537d
Dennis Lira <dlira@stratpoint.com>
2022-10-11 23:28:49 +0800

    Initial setup

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, View, Text, Button, Platform } from "react-native";
+import { FileSystem, Dirs } from "react-native-file-access";
+
+const App = () => {
+
+  const [isConnected, setConnected] = useState(false);
+
+  useEffect(() => {
+    console.log('Use Effect');
+    console.log(Platform.OS);
+    console.log(Dirs.DocumentDir);
+  }, []);
+
+  const handleBtnPress = () => {
+    console.log('Button press');
+
+    const wss = new WebSocket("ws://192.168.1.6:8989");
+    wss.binaryType = "blob";
+    wss.onmessage = (evt) => {
+      //console.log('Message: ', evt.data.toString());
+      if(evt.data instanceof Blob) {
+        
+        const fr = new FileReader();
+        fr.readAsDataURL(evt.data);
+        fr.onload = async () => {
+          const fileUrl = `${Dirs.DocumentDir}/sample_file.tmp`;
+          const data = fr.result.split('base64,')[1];
+          
+          await FileSystem.appendFile(fileUrl, data, 'base64')
+            .then(() => {
+              console.log(`Saved file: ${fileUrl}`);
+            })
+            .catch((e) => {
+              console.log('[FileSystem-Error]: ', e);
+            });
+        };
+        fr.onloadend = () => {
+          console.log('File reader end');
+        };
+
+      } else {
+        console.log("Type is not Blob");
+      }
+    };
+    wss.onopen = () => {
+      console.log('Connected');
+      setConnected(true);
+      /**
+       * Send message
+       */
+      wss.send('Hello from react native android');
+    };
+    wss.onerror = (e) => {
+      console.log('ErrorXD: ', e.message);
+    };
+    wss.onclose = () => {
+      console.log('Connection is closed');
+      setConnected(false);
+    }
+
+  };
+
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Chat Client</Text>
+        <Text>Status: { isConnected ? 'Connected':'Disconnected' }</Text>
+        <Button title="Connect to Server" onPress={() => handleBtnPress() }/>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
